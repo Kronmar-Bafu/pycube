1e5c79cd678eceb6f3dc7bccb2c68c4e326cda78
marco.kronenberg@bafu.admin.ch <kronenberg.marco@gmail.com>
2024-11-05 07:32:30 +0100

    started work on lower/upper uncertainty. Implemented test - it fails

---
+++ py_cube/tests/test_cube.py
@@ -0,0 +1,81 @@
+from py_cube import Cube
+import pandas as pd
+import pytest
+import yaml
+
+class TestClass:
+
+    def setup_method(self):
+        with open("py_cube/tests/test.yml") as file:
+            cube_yaml = yaml.safe_load(file)
+        test_df = pd.read_csv("py_cube/tests/test_data.csv")
+        self.cube = Cube(
+            dataframe=test_df, cube_yaml=cube_yaml,
+            environment="TEST", local=True
+        )
+
+    def test_upper_uncertainty(self):
+        sparql = (
+            "prefix cube: <https://cube.link/>"
+            "prefix dcterms: <http://purl.org/dc/terms/>"
+            "prefix meta: <https://cube.link/meta/> "
+            "prefix mock: <https://mock.ld.admin.ch/>"
+            "prefix qudt: <http://qudt.org/schema/qudt/>"
+            "prefix relation: <https://cube.link/relation/>"
+            "prefix schema: <http://schema.org/>"
+            "prefix sh: <http.//w3.org/ns/shacl#>"
+            "prefix unit: <http://qudt.org/vocab/unit/>"
+            ""
+            "ASK"
+            "{"
+            "  ?shape a cube:Constraint ;"
+            "    sh:property ?prop ."
+            "  ?prop schema:name \'Upper Unsicherheit\'@de ;"
+            "    schema:description \'Upper Unsicherheit\'@de ;"
+            "    sh:path mock:upperUncertainty ;"
+            "    sh:scale sh:ratio ;"
+            "    qudt:hasUnit unit:PERCENT ;"
+            "    meta:dimensionRelation ["
+            "      a relation:ConfidenceUpperBound ;"
+            "      dcterms:type \"Confidence interval\" ;"
+            "      meta:relatesTo mock:value2 ;"
+            "    ] ."
+            "}"
+        )
+
+        result = self.cube._graph.query(sparql)
+        assert bool(result)
+
+    def test_lower_uncertainty(self):
+        sparql = (
+            "prefix cube: <https://cube.link/>"
+            "prefix dcterms: <http://purl.org/dc/terms/>"
+            "prefix meta: <https://cube.link/meta/> "
+            "prefix mock: <https://mock.ld.admin.ch/>"
+            "prefix qudt: <http://qudt.org/schema/qudt/>"
+            "prefix relation: <https://cube.link/relation/>"
+            "prefix schema: <http://schema.org/>"
+            "prefix sh: <http.//w3.org/ns/shacl#>"
+            "prefix unit: <http://qudt.org/vocab/unit/>"
+            ""
+            "ASK"
+            "{"
+            "  ?shape a cube:Constraint ;"
+            "    sh:property ?prop ."
+            "  ?prop schema:name \'Lower Unsicherheit\'@de ;"
+            "    schema:description \'Lower Unsicherheit\'@de ;"
+            "    sh:path mock:lowerUncertainty ;"
+            "    sh:scale sh:ratio ;"
+            "    qudt:hasUnit unit:PERCENT ;"
+            "    meta:dimensionRelation ["
+            "      a relation:ConfidenceLowerBound ;"
+            "      dcterms:type \"Confidence interval\" ;"
+            "      meta:relatesTo mock:value2 ;"
+            "    ] ."
+            "}"
+        )
+
+        result = self.cube._graph.query(sparql)
+        assert bool(result)
+
+
