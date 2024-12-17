0f015e23304219e64ad69557d9bf6e8b7cbf2964
Kronmar-Bafu <marco.kronenberg@bafu.admin.ch>
2024-12-17 11:42:29 +0100

    fixed bugs

--- linpy/__init__.py
+++ linpy/__init__.py
@@ -3,3 +3,5 @@ from linpy.getter.get import get_cube, get_observations
 from linpy.lindas.namespaces import Namespaces
 from linpy.lindas.upload import upload_ttl
 from linpy.lindas.query import query_lindas
+
+__version__ = "0.3.0"
