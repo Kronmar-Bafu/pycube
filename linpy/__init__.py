466e3c61ce3b2bf81021224308403e665c700259
Kronmar-Bafu <Kronmar-Bafu@users.noreply.github.com>
2024-12-17 16:20:34 +0000

    Bump version for release

--- linpy/__init__.py
+++ linpy/__init__.py
@@ -4,4 +4,4 @@ from linpy.lindas.namespaces import Namespaces
 from linpy.lindas.upload import upload_ttl
 from linpy.lindas.query import query_lindas
 
-__version__ = "0.3.7"
+__version__ = "0.4.0"
